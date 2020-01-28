5ef6f76a99b21a46121a0f0c9b22077fad1a1f28
Tan Kheng Hau <tankh@pop-os.localdomain>
2020-01-28 23:56:44 +0800

    initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:chatapp/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Frenzy Chat"),
+        ),
+        body: new ChatScreen());
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:chatapp/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(title: "Chat App", home: new HomePage());
+  }
+}
